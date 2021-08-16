59de492c36d77c9de47089036b8e59d0394b4f7b
cruxcode <swaroopshyam0@gmail.com>
2021-08-16 16:22:53 -0400

    added endpoints

---
+++ src/api/user.ts
@@ -0,0 +1,53 @@
+export const usernameExists = (username: string) => {
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "POST",
+		body: JSON.stringify({ username }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch("http://localhost:4000" + "/user-exists", options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => console.log(err));
+	});
+};
+
+export const register = (username: string, password: string) => {
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "POST",
+		body: JSON.stringify({ username, password }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch("http://localhost:4000" + "/register", options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => console.log(err));
+	});
+};
+
+export const login = (username: string, password: string) => {
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "POST",
+		body: JSON.stringify({ username, password }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch("http://localhost:4000" + "/login", options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => console.log(err));
+	});
+};
