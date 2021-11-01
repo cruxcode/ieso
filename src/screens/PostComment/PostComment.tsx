e3b116332f5d55773e540ebe9bcf05be2f401f17
cruxcode <swaroopshyam0@gmail.com>
2021-11-01 22:47:07 +0530

    post can be viewed by moderator or poster

--- src/screens/PostComment/PostComment.tsx
+++ src/screens/PostComment/PostComment.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { getApprovedPost } from "../../api/posts";
+import { getApprovedPost, getPost } from "../../api/posts";
 import { ShowPost } from "../../components/ShowPost";
 
 const useQuery = () => {
@@ -10,7 +10,16 @@ export const PostComment: React.FC = (props) => {
 	const postID = useQuery().get("postID");
 	const [post, setPost] = useState<any>();
 	useEffect(() => {
-		if (postID)
+		const token = localStorage.getItem("token");
+		if (token && postID) {
+			getPost(postID, token)
+				.then((post: any) => {
+					if (post.success) setPost(post.post);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else if (postID)
 			getApprovedPost(postID)
 				.then((post: any) => {
 					if (post.success) setPost(post.post);
