86e1003d7f103726f23e4c0e358f1ab1a803719f
cruxcode <swaroopshyam0@gmail.com>
2021-11-01 22:01:28 +0530

    comment api added

---
+++ src/api/comments.ts
@@ -0,0 +1,91 @@
+export const sendComment = (comment: string, postID: string) => {
+	const token = localStorage.getItem("token");
+	if (!token) {
+		console.log("not signed in");
+		return;
+	}
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "POST",
+		body: JSON.stringify({ comment, postID, token }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch(`${process.env.REACT_APP_BACKEND_ORIGIN_POST}`, options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => {
+				console.log(err);
+				rej(err);
+			});
+	});
+};
+
+export const castCommentApproval = (
+	commentID: string,
+	status: "approve" | "reject"
+) => {
+	const token = localStorage.getItem("token");
+	if (!token) {
+		console.log("not signed in");
+		return;
+	}
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "PUT",
+		body: JSON.stringify({ commentID, token, status }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch(`${process.env.REACT_APP_BACKEND_ORIGIN_POST}/approval`, options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => {
+				console.log(err);
+				rej(err);
+			});
+	});
+};
+
+export const getApprovedComments = (postID: string) => {
+	var headers = new Headers();
+	headers.append("Content-Type", "application/json");
+	const options = {
+		method: "POST",
+		body: JSON.stringify({ postID }),
+		headers: headers,
+	};
+	return new Promise((res, rej) => {
+		fetch(`${process.env.REACT_APP_BACKEND_ORIGIN_POST}`, options)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => {
+				console.log(err);
+				rej(err);
+			});
+	});
+};
+
+export const getComment = (commentID: string) => {
+	return new Promise((res, rej) => {
+		fetch(
+			`${process.env.REACT_APP_BACKEND_ORIGIN_POST}/comment?commentID=${commentID}`
+		)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				res(resp);
+			})
+			.catch((err) => {
+				console.log(err);
+				rej(err);
+			});
+	});
+};
